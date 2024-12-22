0bc4d0bbba866922866e90d1cd465c94a3163024
andres990420 <72096387+andres990420@users.noreply.github.com>
2024-12-21 22:37:58 -0400

    Add Bootstrap al proyecto
    se le agrego las clases de bootstrap al proyecto para mejorar la funcionalidad

--- clase8/tarea-8.js
+++ clase8/tarea-8.js
@@ -59,17 +59,32 @@ function limpiarDiv()
 function limpiarFormulario()
 {
     $botonEnviar.disabled = false;
-    $botonReset.hidden = true;
-    $botonCalcular.hidden = true;
-    $promedioEdad.hidden = true;
-    $mayorEdad.hidden = true;
-    $menorEdad.hidden = true;
     $cantidadPersonas.value = null;
-    $mayorSalario.hidden = true;
-    $menorSalario.hidden = true;
-    $promedioSalario.hidden = true;
-    $promedioSalarioMensual.hidden = true;
+
+    $botonCalcular.classList.remove('btn');
+    $botonCalcular.classList.remove('btn-primary');
+    $botonReset.classList.remove('btn');
+    $botonReset.classList.remove('btn-danger');
+
+    ocultarElementos();
 };
+
+function ocultarElementos() {
+    document.querySelectorAll('.visible').forEach((elemento) => {
+        elemento.classList.remove('visible');
+        elemento.classList.add('invisible');
+    }
+    );
+}
+
+function mostrarElementos() {
+    document.querySelectorAll('.invisible').forEach((elemento) => {
+        elemento.classList.add('visible');
+        elemento.classList.remove('invisible');
+    }
+    );
+}
+
 
 function validarInputNumeroEntero(numero)
 {
@@ -98,77 +113,6 @@ function validarSalario(salario)
     return '';
 }
 
-// Funcion que le indica al usuario cuando hay un error en el input de cantidad de personas 
-// y se lo muestra en un label
-function manejarErroresNumerosDePesonas(errorNumeroDePersonas) 
-{
-    const error = errorNumeroDePersonas;
-    if(error)    
-    {
-        if(!document.querySelector('#label-error-numero-personas'))
-        {
-            const $errorLabelNumeroPersonas = document.createElement("label");
-            $errorLabelNumeroPersonas.id = `label-error-numero-personas`
-            $errorLabelNumeroPersonas.innerText = ' ' + error;
-            $botonEnviar.insertAdjacentElement('afterEnd' , $errorLabelNumeroPersonas)
-        }else
-        {
-            document.querySelector('#label-error-numero-personas').innerText = ' ' + error;
-        }
-    }else
-    {
-        
-        if(document.querySelector('#label-error-numero-personas'))
-        {
-            document.querySelector('#label-error-numero-personas').remove();
-        }
-        
-    }
-}
-
-//Funcion que maneja los label que contiene los errores en los input de edad
-function manejarLabelErrorEdad(indice, existeError) 
-{
-    const hayError = existeError;
-    if(hayError)
-    {
-        if (!document.getElementById(`error-edad-${indice}`))
-        {
-            const errorLabelEdad = document.createElement('label')
-            errorLabelEdad.id = `error-edad-${indice}`
-            errorLabelEdad.innerText = validarInputNumeroEntero(Number(document.getElementById(`edad-${indice}`).value))
-            document.getElementById(`edad-${indice}`).insertAdjacentElement('afterEnd' , errorLabelEdad)
-        }
-    }else
-    {
-        if(document.getElementById(`error-edad-${indice}`))
-        {
-            document.getElementById(`error-edad-${indice}`).remove()
-        }
-    }
-}
-
-// Funcion que maneja los label que contiene los errores en los input de salarios
-function manejarLabelErrorSalario(indice, existeError) 
-{
-    const hayError = existeError;
-    if(hayError)
-    {
-        if (!document.getElementById(`error-salario-${indice}`))
-        {
-            const errorLabelSalario = document.createElement('label')
-            errorLabelSalario.id = `error-salario-${indice}`
-            errorLabelSalario.innerText = validarSalario(Number(document.getElementById(`input-salario-${indice}`).value))
-            document.getElementById(`input-salario-${indice}`).insertAdjacentElement('afterEnd' , errorLabelSalario)
-        }
-    }else
-    {
-        if(document.getElementById(`error-salario-${indice}`))
-        {
-            document.getElementById(`error-salario-${indice}`).remove()
-        }
-    }
-}
 
 // funcion que se encarga de manejar los errores para los input de salario y edad
 function manejadorErrores(numeroIteraciones) 
@@ -176,30 +120,30 @@ function manejadorErrores(numeroIteraciones)
     let contadorErrores = 0;
     for (let i = 0; i < numeroIteraciones; i++)
     {
-        if(validarInputNumeroEntero(Number(document.getElementById(`edad-${i}`).value)) !== '')
+        const errorEdad = validarInputNumeroEntero(Number(document.getElementById(`edad-${i}`).value))
+        if( errorEdad !== '')
         {
-            document.getElementById(`edad-${i}`).className = 'error'
-            manejarLabelErrorEdad(i,true);
+            document.getElementById(`edad-${i}`).classList.add('is-invalid')
+            document.getElementById(`invalid-edad-${i}`).innerText = errorEdad;
             contadorErrores++;
         }
         else
         {
-            manejarLabelErrorEdad(i,false);
-            document.getElementById(`edad-${i}`).className = ''
+            document.getElementById(`edad-${i}`).classList.remove('is-invalid')
         }
         
         if(document.getElementById(`input-salario-${i}`))
         {
-            if (validarSalario(Number(document.getElementById(`input-salario-${i}`).value)) != '')
+            const errorSalario = validarSalario(Number(document.getElementById(`input-salario-${i}`).value))
+            if ( errorSalario != '')
             {
-                document.getElementById(`input-salario-${i}`).className = 'error'
-                manejarLabelErrorSalario(i,true);
+                document.getElementById(`input-salario-${i}`).classList.add('is-invalid');
+                document.getElementById(`invalid-salario-${i}`).innerText = errorSalario;
                 contadorErrores++;
             }
             else if(document.getElementById(`input-salario-${i}`).value !== ' ')
             {
-                manejarLabelErrorSalario(i,false);
-                document.getElementById(`input-salario-${i}`).className = ''
+                document.getElementById(`input-salario-${i}`).classList.remove('is-invalid');
             }
         }
     }
@@ -221,37 +165,62 @@ const $divEdades = document.querySelector("#edades");
 const $cantidadPersonas = document.querySelector("#cantidad-personas");
 
 
+
 $botonEnviar.onclick = function()
 {
     const errorNumeroDePersonas = validarInputNumeroEntero(Number($cantidadPersonas.value))
     if(errorNumeroDePersonas !== '')
     {
-        $cantidadPersonas.className = 'error';
-        manejarErroresNumerosDePesonas(errorNumeroDePersonas);
+        $cantidadPersonas.classList.add('is-invalid');
+        document.querySelector('#validationNumeroPersonas').innerText = errorNumeroDePersonas;
     }
     else
     {
-        manejarErroresNumerosDePesonas(errorNumeroDePersonas);
+        document.querySelector('#div-card').classList.add('visible');
+        document.querySelector('#div-card').classList.remove('invisible');
+        
+        $cantidadPersonas.classList.remove('is-invalid');
+
         $botonEnviar.disabled = true;
-        $cantidadPersonas.className = '';
-        $botonCalcular.hidden = false;
-        $botonReset.hidden = false;
+        
+        $botonCalcular.classList.remove('invisible');
+        $botonReset.classList.remove('invisible');
+
+        $botonCalcular.classList.add('visible')
+        $botonCalcular.classList.add('btn')
+        $botonCalcular.classList.add('btn-primary')
+        
+        $botonReset.classList.add('visible')
+        $botonReset.classList.add('btn')
+        $botonReset.classList.add('btn-danger')
+
         for(let i=0; i < Number($cantidadPersonas.value); i++)
         {
             const $label = document.createElement("label");
             $label.id = `label-${i}`;
+            $label.classList.add('form-label')
+            $label.classList.add('d-block')
+            $label.classList.add('mt-2')
             $label.textContent = `Ingrese la edad de la persona.`;
             $divEdades.appendChild($label);
             
             const $input = document.createElement("input");
             $input.type = 'Number';
+            $input.classList.add('form-control')
             $input.id = `edad-${i}`
-
             $divEdades.appendChild($input);
+
+            const $invalidEdad = document.createElement("div");
+            $invalidEdad.classList.add('invalid-feedback')
+            $invalidEdad.id = `invalid-edad-${i}`
+            $divEdades.appendChild($invalidEdad)
+
             $divEdades.appendChild(document.createElement("br"));
 
             const $botonAgregar = document.createElement("button");
-            $botonAgregar.className = "boton-agregar";
+            $botonAgregar.classList.add("btn");
+            $botonAgregar.classList.add("btn-outline-primary")
+            $botonAgregar.classList.add("btn-sm");
             $botonAgregar.id = `boton-agregar-${i}`;
             $botonAgregar.textContent = "Agregar Salario";
             $botonAgregar.onclick = agregarLabelsInputs;
@@ -259,14 +228,14 @@ $botonEnviar.onclick = function()
 
             
             const $botonEliminar = document.createElement("button");
-            $botonEliminar.className = "boton-eliminar"
+            $botonEliminar.classList.add('btn')
+            $botonEliminar.classList.add('btn-outline-danger')
+            $botonEliminar.classList.add("btn-sm");
+            $botonEliminar.classList.add('disabled')
             $botonEliminar.id = `boton-eliminar-${i}`;
             $botonEliminar.textContent = "Eliminar Salario";
-            $botonEliminar.disabled = true;
             $botonEliminar.onclick = eliminarLabelsInputs;
             $divEdades.appendChild($botonEliminar);
-        
-            $divEdades.appendChild(document.createElement("br"));
         };
 
         function agregarLabelsInputs()
@@ -275,21 +244,27 @@ $botonEnviar.onclick = function()
             idNumber = idNumber.substring(14);
             
             const $labelSalario = document.createElement("Label");
+            $labelSalario.classList.add('form-label')
+            $labelSalario.classList.add('d-block')
             $labelSalario.id = `label-salario-${idNumber}`;
             $labelSalario.textContent = "Salario anual"
 
             const $inputSalario = document.createElement("input");
+            $inputSalario.classList.add('form-control')
+            $inputSalario.classList.add('mb-2')
             $inputSalario.id = `input-salario-${idNumber}`;
             $inputSalario.type = "number";
+
+            const $invalidSalario = document.createElement("div");
+            $invalidSalario.classList.add('invalid-feedback');
+            $invalidSalario.id = `invalid-salario-${idNumber}`;
             
-            const $breakpage = document.createElement("br");
-            $breakpage.id = `br${idNumber}`
             $divEdades.insertBefore($labelSalario, document.getElementById(`boton-agregar-${idNumber}`));
             $divEdades.insertBefore($inputSalario, document.getElementById(`boton-agregar-${idNumber}`));
-            $divEdades.insertBefore($breakpage, document.getElementById(`boton-agregar-${idNumber}`));
+            $divEdades.insertBefore($invalidSalario, document.getElementById(`boton-agregar-${idNumber}`));
 
-            document.getElementById(`boton-eliminar-${idNumber}`).disabled = false;
-            document.getElementById(`boton-agregar-${idNumber}`).disabled = true;
+            document.getElementById(`boton-eliminar-${idNumber}`).classList.remove('disabled');
+            document.getElementById(`boton-agregar-${idNumber}`).classList.add('disabled');
         };
 
         function eliminarLabelsInputs()
@@ -297,12 +272,11 @@ $botonEnviar.onclick = function()
             let idNumber = this.id;
             idNumber = idNumber.substring(15);
             
-            document.getElementById(`boton-agregar-${idNumber}`).disabled = false;
-            document.getElementById(`boton-eliminar-${idNumber}`).disabled = true;
+            document.getElementById(`boton-agregar-${idNumber}`).classList.remove('disabled');
+            document.getElementById(`boton-eliminar-${idNumber}`).classList.add('disabled');
 
             document.getElementById(`label-salario-${idNumber}`).remove();
             document.getElementById(`input-salario-${idNumber}`).remove();
-            document.getElementById(`br${idNumber}`).remove();
         };
     }    
 }
@@ -330,18 +304,27 @@ $botonCalcular.onclick = function()
         $promedioEdad.textContent = `El promedio de edades es tiene:${calcularPromedio(arrayEdades).toFixed(2)}`;
         $mayorEdad.textContent = `La persona con mayor edad tiene:${calcularMayor(arrayEdades)}`;
         $menorEdad.textContent = `La persona con menor edad tiene:${calcularMenor(arrayEdades)}`;
+        
         $promedioSalario.textContent = `El promedio de los salarios anuales es:${calcularPromedio(arraySalarios).toFixed(2)}`;
         $mayorSalario.textContent = `El mayor salario anual es:${calcularMayor(arraySalarios)}`;
         $menorSalario.textContent = `El menor salario anual es:${calcularMenor(arraySalarios)}`;
         $promedioSalarioMensual.textContent = `El promedio de los salarios mensuales es:${calcularSalarioMensualPromedio(calcularPromedio(arraySalarios)).toFixed(2)}`;
 
-        $promedioEdad.hidden = false;
-        $mayorEdad.hidden = false;
-        $menorEdad.hidden = false;
-        $promedioSalario.hidden = false;
-        $menorSalario.hidden = false;
-        $mayorSalario.hidden = false;
-        $promedioSalarioMensual.hidden = false;
+        if(arraySalarios > 0)
+        {
+            $promedioSalario.classList.remove('d-none');
+            $mayorSalario.classList.remove('d-none');
+            $menorSalario.classList.remove('d-none');
+            $promedioSalarioMensual.classList.remove('d-none');
+        }else
+        {
+            $promedioSalario.classList.add('d-none');
+            $mayorSalario.classList.add('d-none');
+            $menorSalario.classList.add('d-none');
+            $promedioSalarioMensual.classList.add('d-none');
+        }
+        
+        mostrarElementos();
     }
     
 }    
@@ -351,4 +334,5 @@ $botonReset.onclick = function()
     limpiarDiv();
     limpiarFormulario();
 }
+
 
