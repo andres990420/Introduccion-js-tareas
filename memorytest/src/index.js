1921f7774468358224c29f7cc685ba8ec299316b
andres990420 <72096387+andres990420@users.noreply.github.com>
2024-12-27 17:19:35 -0400

    Refacto de la funcion generarColores() y organizacion de código
    Se realizo un refactor a la funcion generarColores() quitando el while por un solo bucle For que se encarga de crear la lista de colores de manera desordenada, tambien se arreglo el estilo del codigo para que fuera mas legible

--- memorytest/src/index.js
+++ memorytest/src/index.js
@@ -1,145 +1,134 @@
-const coloresBase = ['rojo', 'verde', 'amarillo'];
-const $tablero =  document.querySelector('#tablero');
-const $finJuego = document.querySelector('#fin-juego');
-const $botonIniciar = document.querySelector('#iniciar');
-const $botonReiniciar = document.querySelector('#reiniciar');
-const $cuadros = document.querySelectorAll('.cuadro');
+const coloresBase = ["rojo", "verde", "amarillo"];
+const $tablero = document.querySelector("#tablero");
+const $finJuego = document.querySelector("#fin-juego");
+const $botonIniciar = document.querySelector("#iniciar");
+const $botonReiniciar = document.querySelector("#reiniciar");
+const $cuadros = document.querySelectorAll(".cuadro");
 
 let primerCuadro = null;
-let parejasEncontradas = 0; 
+let parejasEncontradas = 0;
 
-$botonReiniciar.onclick = () =>
-{
-    $cuadros.forEach((cuadro)=>
-        {
-            cuadro.classList.remove('invisible');
-            cuadro.classList.remove(cuadro.id);
-            cuadro.id = null;
-            cuadro.parentElement.style.removeProperty('background-color')
-            cuadro.parentElement.style.removeProperty('opacity');
-        })
-    
-    $tablero.classList.remove('invisible');
-    $finJuego.classList.add('invisible');
-    
-    parejasEncontradas = 0;
-    
-    $tablero.onclick = null;
-    
-    $botonIniciar.classList.remove('disabled');
-    $botonReiniciar.classList.add('disabled')
-}
 
-function generarColores(coloresBase)
+function generarColores(coloresBase) 
 {
-    const colores = coloresBase;
+    const colores = coloresBase.slice();
+    const cantidadColores = coloresBase.length;
     let listaColores = [];
-    for(let i=0; i< colores.length; i++)
-        {
-            let loop = true;
-            while (loop)
-            {
-                const index =  Math.floor(Math.random() * (2 - 0 + 1));
-                const color = coloresBase[index];
-                if(!listaColores.includes(color))
-                    {
-                        listaColores.push(color);
-                        loop = false;
-                    };
-            };
-        };
+    for(let i = cantidadColores; i > 0; i--)
+    {
+        const index = Math.floor(Math.random() * (colores.length - 1 - 0 + 1));
+        listaColores.push(colores[index]);
+        colores.splice(index, 1);
+    }
     listaColores = listaColores.concat(listaColores);
-    return listaColores.sort();
+    return listaColores;
 }
 
-$botonIniciar.onclick = function()
+$botonIniciar.onclick = function () 
 {
-    $botonIniciar.classList.add('disabled');
+    $botonIniciar.classList.add("disabled");
     const colores = generarColores(coloresBase);
-    $cuadros.forEach((cuadro)=>
-        {
-            const index = Math.floor(Math.random() * ((colores.length-1) - 0 + 1))
-            cuadro.id = colores[index];
-            colores.splice(index,1);
-        })
-    
-   $tablero.onclick = event =>
+    $cuadros.forEach((cuadro) => 
+    {
+        const index = Math.floor(Math.random() * (colores.length - 1 - 0 + 1));
+        cuadro.id = colores[index];
+        colores.splice(index, 1);
+    });
+
+    $tablero.onclick = (event) => 
     {
         const cuadro = event.target;
         buscarPareja(cuadro);
-    }
-    
-}
+    };
+};
 
-enVerificacion = false;
+let enVerificacion = false;
 
 function buscarPareja(cuadroActual) 
 {
     if (cuadroActual.id && !enVerificacion) 
-        { 
-            mostrarCuadro(cuadroActual); 
-            if (primerCuadro === null && cuadroActual.id) 
-                { primerCuadro = cuadroActual; 
-
-                } else { 
-                    enVerificacion = true; 
-                    setTimeout(() => 
-                        { 
-                            verificarPareja(primerCuadro, cuadroActual); 
-                            primerCuadro = null; 
-                            enVerificacion = false;
-                        }, 300); 
-                } 
-            } 
-}
-
-function mostrarCuadro(cuadro)
-{
-    cuadro.classList.add(cuadro.id)
-}
-
-function ocultarCuadros(cuadro1, cuadro2)
-{
-    setTimeout(() => {
-        cuadro1.classList.remove(cuadro1.id)
-        cuadro2.classList.remove(cuadro2.id)
-    },200)
-}
-
-function eliminarPareja(cuadro1, cuadro2)
-{
-    cuadro1.classList.add('invisible');
-    cuadro2.classList.add('invisible');
-    
-    cuadro1.parentElement.style['background-color'] = 'black'
-    cuadro1.parentElement.style.opacity = 0.25
-    
-    cuadro2.parentElement.style['background-color'] = 'black'
-    cuadro2.parentElement.style.opacity = 0.25
-}
-
-function verificarPareja(primerCuadro, segundoCuadro)
-{
-
-    if(primerCuadro.id !== undefined)
+    {
+        mostrarCuadro(cuadroActual);
+        if (primerCuadro === null && cuadroActual.id) 
         {
-            if(primerCuadro === segundoCuadro)
+            primerCuadro = cuadroActual;
+        } 
+        else 
+        {
+            enVerificacion = true;
+            setTimeout(() => 
             {
-                
-            }else if(primerCuadro.id === segundoCuadro.id)
-                {
-                    eliminarPareja(primerCuadro, segundoCuadro);
-                    parejasEncontradas++;
-            
-                    if(parejasEncontradas === coloresBase.length)
-                        {
-                            $finJuego.classList.remove('invisible');
-                            $tablero.classList.add('invisible');
-                            $botonReiniciar.classList.remove('disabled')
-                        }
-                }
+                verificarPareja(primerCuadro, cuadroActual);
+                primerCuadro = null;
+                enVerificacion = false;
+            }, 300);
         }
-    
-        ocultarCuadros(primerCuadro, segundoCuadro);
-    
+    }
 }
+
+function mostrarCuadro(cuadro) 
+{
+  cuadro.classList.add(cuadro.id);
+}
+
+function ocultarCuadros(cuadro1, cuadro2) 
+{
+    setTimeout(() => 
+    {
+        cuadro1.classList.remove(cuadro1.id);
+        cuadro2.classList.remove(cuadro2.id);
+    }, 200);
+}
+
+function eliminarPareja(cuadro1, cuadro2) 
+{
+    cuadro1.classList.add("invisible");
+    cuadro2.classList.add("invisible");
+
+    cuadro1.parentElement.style["background-color"] = "black";
+    cuadro1.parentElement.style.opacity = 0.25;
+
+    cuadro2.parentElement.style["background-color"] = "black";
+    cuadro2.parentElement.style.opacity = 0.25;
+}
+
+function verificarPareja(primerCuadro, segundoCuadro) 
+{
+    if (primerCuadro.id !== null) 
+    {
+        if (primerCuadro.id === segundoCuadro.id && primerCuadro !== segundoCuadro) 
+        {
+            eliminarPareja(primerCuadro, segundoCuadro);
+            parejasEncontradas++;
+
+            if (parejasEncontradas === coloresBase.length) 
+            {
+                $finJuego.classList.remove("invisible");
+                $tablero.classList.add("invisible");
+                $botonReiniciar.classList.remove("disabled");
+            }
+        }
+    }
+    ocultarCuadros(primerCuadro, segundoCuadro);
+}
+
+$botonReiniciar.onclick = () => 
+{
+    $cuadros.forEach((cuadro) => 
+    {
+        cuadro.classList.remove("invisible");
+        cuadro.classList.remove(cuadro.id);
+        cuadro.id = null;
+        cuadro.parentElement.style.removeProperty("background-color");
+        cuadro.parentElement.style.removeProperty("opacity");
+    });
+
+    $tablero.classList.remove("invisible");
+    $finJuego.classList.add("invisible");
+
+    parejasEncontradas = 0;
+
+    $tablero.onclick = null;
+    $botonIniciar.classList.remove("disabled");
+    $botonReiniciar.classList.add("disabled");
+};
